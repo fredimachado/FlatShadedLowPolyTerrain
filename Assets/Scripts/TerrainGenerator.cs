e88f63d320f6412faf6fcedd77075a81f06aaf59
Fred Machado <fredisoft@gmail.com>
2017-10-04 19:35:29 +1100

    Implement simple flat square mesh

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -1,16 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class TerrainGenerator : MonoBehaviour {
+[RequireComponent(typeof(MeshFilter))]
+public class TerrainGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private int size;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Start()
+    {
+        var vertices = new Vector3[(size + 1) * (size + 1)];
+        for (int i = 0, z = 0; z <= size; z++)
+        {
+            for (int x = 0; x <= size; x++, i++)
+            {
+                vertices[i] = new Vector3(x, 0f, z);
+            }
+        }
+
+        var triangles = new int[size * size * 6];
+        for (int ti = 0, vi = 0, y = 0; y < size; y++, vi++)
+        {
+            for (int x = 0; x < size; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + size + 1;
+                triangles[ti + 5] = vi + size + 2;
+            }
+        }
+
+        var mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+
+        var meshFilter = GetComponent<MeshFilter>();
+        meshFilter.sharedMesh = mesh;
+    }
 }
